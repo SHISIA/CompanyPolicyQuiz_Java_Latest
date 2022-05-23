65753163490c97473ba9600f8a768be2c6024ff7
Hiram K. <hiram.kamau@outlook.com>
2022-05-23 09:47:34 +0300

    Create a launcher class for starting application in the absence of JavaFX runtime

---
+++ src/main/java/companypolicyquiz_grouplatest/companypolicyquiz_java_latest/Launcher.java
@@ -0,0 +1,9 @@
+package companypolicyquiz_grouplatest.companypolicyquiz_java_latest;
+
+public class Launcher {
+
+    public static void main(String[] args) {
+        HelloApplication.main(args);
+        // ClientView.main(args);
+    }
+}
